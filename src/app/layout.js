cc94129932267c9cd577eabdfd9fa53e194c43ca
mia <miacarinirojo@gmail.com>
2026-05-25 19:54:02 -0300

    favicon!

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,11 +14,11 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "TMDB Movie and TV Show Browser",
+  title: "Soli - TMDB Browser",
   description: "A web application to browse movies and TV shows using The Movie Database (TMDB) API.",
   icons: {
     icon: "/soli-n.png",
-    shortcut: "/soli-n.png",
+    shortcut: "/soli-n.png", 
   },
 };
 
